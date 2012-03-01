743cdca44ba0adcacd321156e3f4bba12ec715dc
Arnold Sikorski <asikorski@sosoftware.pl>
2012-03-01 15:33:39 +0100

    add zend

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,35 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        //$config = new Zend_Config_Xml(THEMES_PATH.'/simple.xml');
+        $config = simplexml_load_file(THEMES_PATH.'/simple.xml');
+        $arrayConfig = array();
+        foreach( $config->children() as $child ){
+            //new Socms_Dump($child->getName());
+            //$xmldata->attributes()->count() > 0
+            //new Socms_Dump($child->attributes()->count());
+            if($child->attributes()->count()>0){
+                $attributeArray = array();
+                foreach ($child->attributes() as $name => $attrib) {
+                    $attributeArray[$name] = (string)$attrib;
+                }
+                $itemName = $attributeArray['name'];
+                /**/
+                if(!isset($attributeArray['bgColor'])){
+                    $attributeArray['bgColor'] = false;   
+                }
+                if(!isset($attributeArray['foreColor'])){
+                    $attributeArray['foreColor'] = false;   
+                }
+                unset($attributeArray['name']);
+                $arrayConfig[$itemName] = $attributeArray;
+                //new Socms_Dump($attributeArray);
+            }
+            
+        }
+        new Socms_Dump($arrayConfig);
+            
+       //. new Socms_Dump($config);
     }
 
 
--- public/index.php
+++ public/index.php
@@ -8,14 +8,20 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
+defined('THEMES_PATH')
+    || define('THEMES_PATH', realpath(dirname(__FILE__) . '/themes'));
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
     get_include_path(),
 )));
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->setFallbackAutoloader(true);
+$loader->suppressNotFoundWarnings(false);
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
